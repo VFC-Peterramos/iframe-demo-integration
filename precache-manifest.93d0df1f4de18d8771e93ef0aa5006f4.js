1769e51a8cf561a284c781b8a0361bd67ea5438d
VFC-Peterramos <peter.ramos@covetrus.com>
2020-09-22 10:26:38 -0400

    Updates

---
+++ precache-manifest.93d0df1f4de18d8771e93ef0aa5006f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a829f259b3e694221cef94f8b068c3",
+    "url": "/iframe-demo-integration/index.html"
+  },
+  {
+    "revision": "71c6bc58be8ad796c70b",
+    "url": "/iframe-demo-integration/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "626dd2e545522919cfaa",
+    "url": "/iframe-demo-integration/static/js/2.1bceacfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iframe-demo-integration/static/js/2.1bceacfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c6bc58be8ad796c70b",
+    "url": "/iframe-demo-integration/static/js/main.800ae02d.chunk.js"
+  },
+  {
+    "revision": "2c99a061bb7a05c66d77",
+    "url": "/iframe-demo-integration/static/js/runtime-main.c2266c75.js"
+  }
+]);
